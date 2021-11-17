a7da0f58287ee4b048b24921011105ad6524604b
Alejo Madrid <alejandro.madrid@globant.com>
2021-11-16 23:47:44 -0500

    Test runninmg before clean up

--- src/test/java/test/DoubleEmailTest.java
+++ src/test/java/test/DoubleEmailTest.java
@@ -25,18 +25,15 @@ public class DoubleEmailTest {
 
         Response responseGet = given().when().get(endpoint);
 
-        for (int i = 1; i < duplicateEmail.length; i++) {
-            for (int j = 1; j < duplicateEmail.length; j++) {
+        for (int i = 0; i < duplicateEmail.length; i++) {
+            for (int j = i + 1; j < duplicateEmail.length; j++) {
                 if(duplicateEmail[i].getEmail().equals(duplicateEmail[j].getEmail())){
                     System.out.println("there is a repeated email " + duplicateEmail[i].getEmail() + " No " + i + " " + duplicateEmail[j].getEmail() + " No " + j);
                 } else {
                     responseGet.then().assertThat().statusCode(200);
                 }
-
             }
-
         }
-
 
     }
 
--- src/test/java/test/UpdateAccountNumberTest.java
+++ src/test/java/test/UpdateAccountNumberTest.java
@@ -1,7 +1,10 @@
 package test;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import pojo.BankDataPOJO;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,33 +13,53 @@ import static io.restassured.RestAssured.given;
 
 public class UpdateAccountNumberTest {
 
+    /**
+     * @Test4 > Add a test to update an existing AccountNumber
+     *
+     *
+     * @autor Alejandro Madrid
+     */
+
     @Test
-    public void updateTest(){
-//      Given
-        String endpoint = "https://6192a21bd3ae6d0017da8217.mockapi.io/Users/6";
+    @Parameters({"endpoint"})
+    public void updateTest(String endpoint){
 
-        Response response;
-        Map<String, String> Users = new HashMap<String, String>();
-        Users.put("Name", "Giraldillo");
-        Users.put("LasName", "Apellido de Jaramillo");
-        Users.put("AccountNumber", "28471380");
-        Users.put("Amount", "681.49");
-        Users.put("TransactionType", "invoice");
-        Users.put("Email", "Lamar6@yahoo.com");
-        Users.put("Active", "false");
-        Users.put("Country", "Indonesia");
-        Users.put("Telephone", "(563) 449-1300 x50151");
+        Response updateAccount;
 
-        //When
-        response = given().contentType("application/json").body(Users).when().put(endpoint);
+        BankDataPOJO setUpdateAccount = new BankDataPOJO();
+        setUpdateAccount.setAccountNumber("66587043663");
 
-//      Then
+        updateAccount = given().contentType(ContentType.JSON).body(setUpdateAccount).when().put(endpoint + "/" + 1);
 
+        updateAccount.then().assertThat().statusCode(200);
 
-
-        response.prettyPrint();
-
-//        response.then().assertThat().statusCode(200);
+        updateAccount.prettyPrint();
 
     }
 }
+
+//      Given
+//        String endpoint = "https://6192a21bd3ae6d0017da8217.mockapi.io/Users/6";
+//
+//        Response response;
+//        Map<String, String> Users = new HashMap<String, String>();
+//        Users.put("Name", "Giraldillo");
+//        Users.put("LasName", "Apellido de Jaramillo");
+//        Users.put("AccountNumber", "28471380");
+//        Users.put("Amount", "681.49");
+//        Users.put("TransactionType", "invoice");
+//        Users.put("Email", "Lamar6@yahoo.com");
+//        Users.put("Active", "false");
+//        Users.put("Country", "Indonesia");
+//        Users.put("Telephone", "(563) 449-1300 x50151");
+//
+//        //When
+//        response = given().contentType("application/json").body(Users).when().put(endpoint);
+//
+////      Then
+//
+//
+//
+//        response.prettyPrint();
+//
+////        response.then().assertThat().statusCode(200);
